5fcbd0a204a0ea0b80d2ac2fa4f20d5f6d2f3c8a
andermena23 <157723810+andermena23@users.noreply.github.com>
2024-12-10 09:23:49 +0000

    initial commit lab

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const maxSize = 2 * 1024 * 1024; // 2 * 1MB
+
+const formatFilter = function (req, file, cb) {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Soilik irudi fitxategiak onartzen dira!'), false);
+    }
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const fileExtension = file.originalname.split('.').pop();
+        const fileNameWithoutExtension = file.originalname.replace(`.${fileExtension}`, '');
+        cb(null, `${fileNameWithoutExtension}-${uniqueSuffix}.${fileExtension}`)
+    }
+});
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: formatFilter,
+    limits: { fileSize: maxSize }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +40,5 @@ router.post('/', upload.single('avatar'), function (req, res, next) {
     // req.body will hold the text fields, if there were any
     res.send("Jasota")
 })
-
 
 module.exports = router;
